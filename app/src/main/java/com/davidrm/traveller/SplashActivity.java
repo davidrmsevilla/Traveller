e06f599ef6438471281a3a53491674356b449edd
David <davidrmsevilla@hotmail.com>
2020-05-25 18:27:05 -0400

    first commit

---
+++ app/src/main/java/com/davidrm/traveller/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.davidrm.traveller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Clase que nos permite hacer una espera de 2000ms
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },6000);
+
+
+    }
+}
